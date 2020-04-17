bf8443deb6887e6ba4915763820f9767a228642b
firestatus <40839417+nonplated@users.noreply.github.com>
2020-04-17 20:45:12 +0200

    background image added with blur effect

--- js/module.js
+++ js/module.js
@@ -1,8 +1,5 @@
 //In file js/answers.js will be created array: answers
 
-function $(element_id) {
-    return document.getElementById(element_id);
-}
 
 function removeDups(names) {
     let unique = {};
@@ -39,45 +36,45 @@ function getClasses(year) {
 
 function createLinksForYears() {
     let y = getYears();
-    let links_years = $("links_years");
+    let $links_years = document.getElementById("links_years");
     y.forEach(year => {
         a = document.createElement("a");
         a.title = year;
         a.href = "#";
         a.innerHTML = year;
         a.addEventListener("click", function() {
-            $("container_classes").style.display = "flex";
-            $("year").value = year;
-            $("class_nr").value = "";
-            links_years.childNodes.forEach(node => {
+            document.getElementById("container_classes").style.display = "flex";
+            document.getElementById("year").value = year;
+            document.getElementById("class_nr").value = "";
+            $links_years.childNodes.forEach(node => {
                 node.className = "unselected";
             });
             this.className = "selected";
             createLinksForClasses(year);
         });
-        links_years.appendChild(a);
+        $links_years.appendChild(a);
     });
 }
 
 function createLinksForClasses(year) {
     let y = getClasses(year);
-    let links_classes = $("links_classes");
-    links_classes.innerHTML = "";
+    let $links_classes = document.getElementById("links_classes");
+    $links_classes.innerHTML = "";
     y.forEach(class_nr => {
         a = document.createElement("a");
         a.title = class_nr;
         a.href = "#";
         a.innerHTML = class_nr;
         a.addEventListener("click", function() {
-            $("container_button").style.display = "flex";
-            $("container_answers").style.display = "flex";
-            $("class_nr").value = class_nr;
-            links_classes.childNodes.forEach(node => {
+            document.getElementById("container_button").style.display = "flex";
+            document.getElementById("container_answers").style.display = "flex";
+            document.getElementById("class_nr").value = class_nr;
+            $links_classes.childNodes.forEach(node => {
                 node.className = "unselected";
             });
             this.className = "selected";
         });
-        links_classes.appendChild(a);
+        $links_classes.appendChild(a);
     });
 }
 
@@ -105,25 +102,25 @@ function createCodeToScan(text) {
 }
 
 function checkAnswers() {
-    let year = $("year").value;
-    let class_nr = $("class_nr").value;
-    let your_answers = $("answers").value;
-    let warnings = $("warnings");
+    let year = document.getElementById("year").value;
+    let class_nr = document.getElementById("class_nr").value;
+    let your_answers = document.getElementById("answers").value;
+    let $warnings = document.getElementById("warnings");
     let good_counter = 0;
-    let bad_answers = new Array();
+    let bad_answers = [];
     let is_error = false;
 
-    warnings.innerHTML = "";
+    $warnings.innerHTML = "";
     if (!year) {
-        warnings.innerHTML += "<p>Select year</p>";
+        $warnings.innerHTML += "<p>Select year</p>";
         is_error = true;
     }
     if (!class_nr) {
-        warnings.innerHTML += "<p>Select class</p>";
+        $warnings.innerHTML += "<p>Select class</p>";
         is_error = true;
     }
     if (your_answers.length !== 30) {
-        warnings.innerHTML += "<p>Your answer string must have 30 chars.</p>";
+        $warnings.innerHTML += "<p>Your answer string must have 30 chars.</p>";
         is_error = true;
     }
     if (is_error) {
@@ -131,8 +128,8 @@ function checkAnswers() {
     }
 
     //in default, dont show results (maybe nothing to show)
-    $("results").style.display = "none";
-    $("form1").style.display = "flex";
+    document.getElementById("results").style.display = "none";
+    document.getElementById("form1").style.display = "flex";
 
     good_answers = getGoodAnswers(year, class_nr);
     if (good_answers) {
@@ -155,11 +152,11 @@ function checkAnswers() {
                 );
             }
         }
-        $("good_answers").innerHTML =
+        document.getElementById("good_answers").innerHTML =
             good_counter + " / " + good_answers.length;
-        $("bad_answers").innerHTML = bad_answers.join(" ") || "none";
-        $("results").style.display = "flex";
-        $("form1").style.display = "none";
+            document.getElementById("bad_answers").innerHTML = bad_answers.join(" ") || "none";
+        document.getElementById("results").style.display = "flex";
+        document.getElementById("form1").style.display = "none";
 
         // code for print and scan
         if (good_counter === good_answers.length) {
@@ -169,11 +166,11 @@ function checkAnswers() {
 
             let p = document.createElement("p");
             p.innerHTML = good_answers + "  (" + year + ", " + class_nr + ")";
-            $("printed").appendChild(p);
+            document.getElementById("printed").appendChild(p);
 
-            $("print_code").style.display = "block";
+            document.getElementById("print_code").style.display = "block";
         } else {
-            $("print_code").style.display = "none";
+            document.getElementById("print_code").style.display = "none";
         }
     } else {
         console.log("ERROR: Not found YEAR and CLASS_NR in answers.");
@@ -182,8 +179,8 @@ function checkAnswers() {
 }
 
 function countChars() {
-    let s = $("answers");
-    if (s) {
-        $("chars_counter").innerHTML = s.value.length.toString() + "/30";
+    let $s = document.getElementById("answers");
+    if ($s) {
+        document.getElementById("chars_counter").innerHTML = $s.value.length.toString() + "/30";
     }
 }
